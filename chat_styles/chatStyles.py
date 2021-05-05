c1ad28e696b4a242d6b93963766249a2352e0ca3
Nzparra <nzparra@gmail.com>
2021-05-04 23:56:38 -0500

    FIX Styles

--- chat_styles/chatStyles.py
+++ chat_styles/chatStyles.py
@@ -1,7 +1,8 @@
 from tkinter import *
+from images import img
 
 BG_GRAY = "#ABB2B9"
-BG_COLOR = PhotoImage(file = "C:\\Users\\location\\imageName.png")
+BG_COLOR = "#696969"
 TEXT_COLOR = "#EAECEE"
 FONT = "Helveltica 14"
 FONT_BOLD = "Helveltica 13 bold"
