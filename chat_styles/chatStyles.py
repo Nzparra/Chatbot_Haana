066fe050279a34ee2ded8e7eb8298ed2ec7b69d4
Nzparra <nzparra@gmail.com>
2021-05-05 16:22:12 -0500

    Update styles

--- chat_styles/chatStyles.py
+++ chat_styles/chatStyles.py
@@ -2,8 +2,10 @@ from tkinter import *
 from images import img
 
 BG_GRAY = "#ABB2B9"
-BG_COLOR = "#696969"
+BG_COLOR = "#808080"
 TEXT_COLOR = "#EAECEE"
-FONT = "Helveltica 14"
-FONT_BOLD = "Helveltica 13 bold"
+FONT = "Courier 14"
+FONT_BOLD = "Courier 13 bold"
 PADY = 10
+BT_COLOR = "#FFA500"
+BT_TEXT = "#FFFFFF"
